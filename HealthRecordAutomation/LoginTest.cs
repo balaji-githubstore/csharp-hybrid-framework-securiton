7342ee86b799738d7d851ef8ab4167061b55c966
Balaji <balaji0017@gmail.com>
2023-08-23 11:40:32 +0530

    basic framework setup

---
+++ HealthRecordAutomation/LoginTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Securiton.Base;
+using OpenQA.Selenium.Support.UI;
+
+namespace Securiton.HealthRecordAutomation
+{
+    public class LoginTest : AutomationWrapper
+    {
+        
+        [Test]
+        public void ValidLoginTest()
+        {
+            driver.FindElement(By.Id("authUser")).SendKeys("physician");
+            driver.FindElement(By.CssSelector("#clearPass")).SendKeys("physician");
+            SelectElement selectLanguage = new SelectElement(driver.FindElement(By.CssSelector("select[name='languageChoice']")));
+            selectLanguage.SelectByText("English (Indian)");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            //wait for page load to complete 
+            Assert.That(driver.Title, Is.EqualTo("OpenEMR"));
+        }
+
+
+    }
+}
---
+++ HealthRecordAutomation/LoginUITest.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Securiton.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Securiton.HealthRecordAutomation
+{
+    
+    public class LoginUITest : AutomationWrapper
+    {
+        [Test]
+        public void VerifyTitleTest()
+        {
+            string actualTitle = driver.Title;
+            Assert.That(actualTitle, Is.EqualTo("OpenEMR Login"));
+        }
+
+        [Test]
+        public void VerifyPlaceholderTest()
+        {
+          string actualUserNamePlaceholder=  driver.FindElement(By.Id("authUser")).GetAttribute("placeholder");
+          string actualPasswordPlaceholder = driver.FindElement(By.CssSelector("#clearPass"))
+                .GetAttribute("placeholder");
+
+            Assert.That(actualUserNamePlaceholder, Is.EqualTo("Username"));
+            Assert.That(actualPasswordPlaceholder, Is.EqualTo("Password"));
+        }
+
+    }
+}
